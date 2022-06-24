1eaf8182e63bffcd2c7280ced94ed8187e5350b6
Simo Aleksandrov <simo3003@me.com>
2022-06-24 18:46:17 +0300

    feat: frontend demo

--- frontend/src/routes/chain/index.ts
+++ frontend/src/routes/chain/index.ts
@@ -1,14 +1,16 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
 const chainEndpoint = new URL('/', process.env.BACKEND_URL).href;
 
-export const get = async (input?: string) => {
-    const url = new URL(chainEndpoint)
-    if (input) {
-        url.searchParams.set("in", input)
-    }
+export const get: RequestHandler = async ({ url: { searchParams } }) => {
+  const url = new URL(chainEndpoint);
+  const input = searchParams.get('input');
+  if (input) {
+    url.searchParams.set('in', input);
+  }
+  const response = await fetch(url.toString()).then((res) => res.text());
 
-    const response = await fetch(url.toString()).then((res) => res.text());
-
-    return {
-        body: response,
-    };
+  return {
+    body: response,
+  };
 };
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,26 +1,57 @@
 <script context='module'>
-  // export const load = async ({ fetch }) => {
-  // const stats = await fetch('/stats').then((res) => res.json());
-  //
-  // return {
-  //   props: {
-  //     topScorers: stats.slice(0, 3),
-  //     rows: stats.slice(3).map((stat) => ({
-  //       ...stat,
-  //       username: stat.username.split('#')[0],
-  //       id: stat.username,
-  //     })),
-  //   },
-  // };
-  // };
+  export const load = async ({ fetch }) => {
+    const response = await fetch('/chain').then((res) => res.text());
+
+    return {
+      props: {
+        messages: [response],
+      },
+    };
+  };
 </script>
 
 <script>
   import 'carbon-components-svelte/css/g90.css';
   import Header from '../components/Header.svelte';
+  import { TextInput } from 'carbon-components-svelte';
+  import { Button } from 'carbon-components-svelte';
+  import NextOutline from 'carbon-icons-svelte/lib/NextOutline.svelte';
+
+  export let messages;
+  let input;
+
+
+  async function handleClick() {
+    const params = new URLSearchParams();
+    if (input !== '') {
+      params.set('input', input);
+    }
+    const response = await fetch(`/chain?${params.toString()}`).then(v => v.text());
+    messages = [response, ...messages].slice(0, 10);
+  }
 </script>
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+    }
+
+    h1 {
+        text-align: center;
+    }
+</style>
 
 <svelte:head>
   <title>kirechain</title>
 </svelte:head>
 <Header />
+<div class='container'>
+  <TextInput placeholder='Start of message' size='xl' bind:value={input} />
+  <Button iconDescription='Go' on:click={handleClick} icon={NextOutline} href='#' kind='secondary'>Generate</Button>
+</div>
+{#each messages as message}
+  <h1>
+    {message}
+  </h1>
+{/each}
